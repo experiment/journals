57c1cde1283e9f72d059bd8f4794922f2b7713f6
Ryan Lower <rpjlower@gmail.com>
2014-04-17 12:17:10 -0700

    Add spec for scraping a plos one paper

--- spec/journals/plos_paper_spec.rb
+++ spec/journals/plos_paper_spec.rb
@@ -4,33 +4,53 @@ describe Journals::PlosPaper do
   let(:paper) { Journals::PlosPaper.new }
 
   describe '#scrape!' do
-    let(:doi) { '10.1371/journal.pgen.1004275' }
-
     before do
       paper.doi = doi
+      VCR.use_cassette ['paper', doi] { paper.scrape! }
+    end
 
-      VCR.use_cassette ['paper', doi] do
-        paper.scrape!
+    describe 'plos one paper' do
+      let(:doi) { '10.1371/journal.pone.0095361' }
+
+      it 'should parse the doi' do
+        paper.doi.must_equal doi
+      end
+
+      it 'should parse the title' do
+        paper.title.must_equal 'Chimpanzees Preferentially Select Sleeping Platform Construction Tree Species with Biomechanical Properties that Yield Stable, Firm, but Compliant Nests'
+      end
+
+      it 'should parse the published_at' do
+        paper.published_at.must_equal Date.parse('2014-04-16')
+      end
+
+      it 'should parse the keywords' do
+        paper.keywords.must_equal ['Chimpanzees', 'Internodes', 'Leaves',
+          'Orangutans', 'Phylogenetic analysis', 'Sleep', 'Stiffness', 'Trees']
       end
     end
 
-    it 'should parse the doi' do
-      paper.doi.must_equal doi
-    end
+    describe 'plos genetics paper' do
+      let(:doi) { '10.1371/journal.pgen.1004275' }
 
-    it 'should parse the title' do
-      paper.title.must_equal 'Interplay of the Serine/Threonine-Kinase StkP and the Paralogs DivIVA and GpsB in Pneumococcal Cell Elongation and Division'
-    end
+      it 'should parse the doi' do
+        paper.doi.must_equal doi
+      end
 
-    it 'should parse the published_at' do
-      paper.published_at.must_equal Date.parse('2014-04-10')
-    end
+      it 'should parse the title' do
+        paper.title.must_equal 'Interplay of the Serine/Threonine-Kinase StkP and the Paralogs DivIVA and GpsB in Pneumococcal Cell Elongation and Division'
+      end
 
-    it 'should parse the keywords' do
-      paper.keywords.must_equal ['Cell cycle and cell division', 'Cell fusion',
-        'Fluorescence imaging', 'Multiple alignment calculation',
-        'Phase contrast microscopy', 'Phosphorylation', 'Pneumococcus',
-        'Sequence alignment']
+      it 'should parse the published_at' do
+        paper.published_at.must_equal Date.parse('2014-04-10')
+      end
+
+      it 'should parse the keywords' do
+        paper.keywords.must_equal ['Cell cycle and cell division', 'Cell fusion',
+          'Fluorescence imaging', 'Multiple alignment calculation',
+          'Phase contrast microscopy', 'Phosphorylation', 'Pneumococcus',
+          'Sequence alignment']
+      end
     end
   end
 end
