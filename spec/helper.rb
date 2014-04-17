9f57fbaf3d44733d300cc6d8e54857a41d319385
Ryan Lower <rpjlower@gmail.com>
2014-04-17 10:30:26 -0700

    Use VCR to record requests

--- spec/helper.rb
+++ spec/helper.rb
@@ -4,6 +4,13 @@ require 'minitest/pride'
 
 require 'mocha/setup'
 
+require 'vcr'
+
 require 'pry'
 
 require 'journals'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+end
--- spec/journals/plos_paper_spec.rb
+++ spec/journals/plos_paper_spec.rb
@@ -4,12 +4,15 @@ describe Journals::PlosPaper do
   let(:paper) { Journals::PlosPaper.new }
 
   describe '#scrape!' do
+    let(:doi) { '10.1371/journal.pgen.1004275' }
     let(:url) { 'http://www.plosgenetics.org/article/info%3Adoi%2F10.1371%2Fjournal.pgen.1004275' }
 
     before do
       paper.url = url
 
-      paper.scrape!
+      VCR.use_cassette ['paper', doi] do
+        paper.scrape!
+      end
     end
 
     it 'works' do
