f7bfe343ceb6bd8f87beb67bb88b7de36893e92a
Ryan Lower <rpjlower@gmail.com>
2014-04-17 17:38:34 -0700

    Add parsing for BMC journal issues

--- lib/journals/bmc_issue.rb
+++ lib/journals/bmc_issue.rb
@@ -4,18 +4,23 @@ module Journals
     PAPER_CLASS = Journals::BmcPaper
 
     def parse
-      self.papers = papers_from(parse_paper_dois)
+      self.papers = papers_from(parse_paper_urls)
     end
 
     private
 
-      def papers_from(dois)
-        dois.map do |doi|
-          PAPER_CLASS.new doi: doi
+      def papers_from(urls)
+        urls.map do |url|
+          PAPER_CLASS.new url: url
         end
       end
 
-      def parse_paper_dois
+      def parse_paper_urls
+        html.css('.articles-feed tr').select do |el|
+          el.css('.article-type').text =~ /^research/i
+        end.map do |el|
+          el.css('.article-title a').first['href']
+        end
       end
   end
 end
--- spec/journals/bmc_issue_spec.rb
+++ spec/journals/bmc_issue_spec.rb
@@ -4,5 +4,31 @@ describe Journals::BmcIssue do
   let(:issue) { Journals::BmcIssue.new }
 
   describe '#parse' do
+    before do
+      issue.url = url
+      VCR.use_cassette ['issue', url] { issue.scrape! }
+    end
+
+    describe 'Parasites & Vectors February 2014 issue' do
+      let(:url) { 'http://www.parasitesandvectors.com/content/7/February/2014' }
+
+      it 'should parse the papers' do
+        issue.papers.count.must_equal 21
+        issue.papers.first.must_be_instance_of Journals::BmcPaper
+        issue.papers.first.url.must_equal 'http://www.parasitesandvectors.com/content/7/1/82'
+        issue.papers.last.url.must_equal 'http://www.parasitesandvectors.com/content/7/1/58'
+      end
+    end
+
+    describe 'BMC Bioinformatics April 2007 issue' do
+      let(:url) { 'http://www.biomedcentral.com/bmcbioinformatics/content/8/April/2007' }
+
+      it 'should parse the papers' do
+        issue.papers.count.must_equal 16
+        issue.papers.first.must_be_instance_of Journals::BmcPaper
+        issue.papers.first.url.must_equal 'http://www.biomedcentral.com/1471-2105/8/141'
+        issue.papers.last.url.must_equal 'http://www.biomedcentral.com/1471-2105/8/113'
+      end
+    end
   end
 end
